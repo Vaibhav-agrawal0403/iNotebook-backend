0c422d7b55f736a162ca825679f98c4f61178e9e
Vaibhav-agrawal0403 <vaibhavagrawal0403@gmail.com>
2023-02-03 02:22:36 +0530

    second

--- routes/auth.js
+++ routes/auth.js
@@ -14,10 +14,12 @@ router.post('/createuser',[
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({ min: 5 })
 ] , async(req,res)=>{
+    let success = false;
+
     // If there are errors, return Bad request and the errors.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array()});
+      return res.status(400).json({success, errors: errors.array()});
     }
     
     try {
@@ -25,7 +27,7 @@ router.post('/createuser',[
 
         // await is use here because it is promises means user take some time to find a user email
         // is present or not in model.
-        let user = await User.findOne({email:req.body.email});
+        let user = await User.findOne({success, email:req.body.email});
         console.log(user)
         if(user){
             return res.status(400).json({error : "Sorry a user with this email already exist"})
@@ -50,7 +52,8 @@ router.post('/createuser',[
         const authtoken = jwt.sign(data, JWT_SECRET);
 
         // res.json(user)
-        res.json({authtoken})
+        success=true;
+        res.json({success, authtoken})
 
     // catch error occured when some written mistake or anything else
     }catch (error) {
@@ -61,18 +64,17 @@ router.post('/createuser',[
 
 
 // Route 2: Authenticate a User using : POST "/api/auth/login". No login required
-router.post('/login'
-// ,
-// [
-//     body('email','Enter a valid email').isEmail(),
-//     body('password','Password cannot be blank').exists()
-// ] 
+router.post('/login',
+[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password cannot be blank').exists()
+] 
 , async(req,res)=>{
-
+    success=false;
     // If there are errors, return Bad request and the errors.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array()});
+      return res.status(400).json({success, errors: errors.array()});
     }
 
     // use destructuring
@@ -83,14 +85,14 @@ router.post('/login'
         let user = await User.findOne({email}); // email is exist in User database or not
         // if user doesn't exist 
         if(!user){
-            return res.status(400).json({error:"Please try to login with correct Credentials"});
+            return res.status(400).json({success, error:"Please try to login with correct Credentials"});
         }
 
         // compare password
         const passwordCompare = await bcrypt.compare(password,user.password)
         // if password not match
         if(!passwordCompare){
-            return res.status(400).json({error:"Please try to login with correct Credentials"});
+            return res.status(400).json({success,error:"Please try to login with correct Credentials"});
         }
 
         const data = {
@@ -99,7 +101,8 @@ router.post('/login'
             }
         }
         const authtoken = jwt.sign(data, JWT_SECRET);
-        res.json({authtoken})
+        success=true;
+        res.json({success,authtoken})
 
     } catch (error) {
         console.log(error.message);
