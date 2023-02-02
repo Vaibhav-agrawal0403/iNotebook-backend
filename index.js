2df9fd6f4e2779cf700530d4c0105e57013775a4
Vaibhav-agrawal0403 <vaibhavagrawal0403@gmail.com>
2023-02-03 00:22:27 +0530

    second

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ app.use(express.json())
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+app.get('/',(req,res)=>res.send('this is backend'))
 
 // step 3: Heroku
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("frontend/build"));
-}
+// if (process.env.NODE_ENV == "production") {
+//   app.use(express.static("frontend/build"));
+// }
 
 app.listen(port, () => {
   console.log(`iNotebook backend listening at http://localhost:${port}`)
